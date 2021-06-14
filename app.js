fd9ea850296914ea55ed553fefb052fe0c9d4f87
Rahul mourya <mouryarahul223@gmail.com>
2021-06-14 09:36:17 +0530

    updated files

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(require('./router/Post'));
 
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.stati("client/build"));
+    app.use(express.static("client/build"));
     const path=require("path");
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
